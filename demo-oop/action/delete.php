25d343f2114b2cbbacf19a66e3e1eb6511f8232d
hkbike <xuanhoa93th@gmail.com>
2020-07-27 13:02:07 +0700

    dfgd

--- demo-oop/action/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include "../class/Student.php";
-include "../class/StudentManager.php";
-
-$index = $_REQUEST['index'];
-$studentManager = new StudentManager("../data/data.json");
-$studentManager->deleteStudent($index);
-header("Location: ../index.php");
